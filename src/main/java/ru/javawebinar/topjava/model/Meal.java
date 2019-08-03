07fab1538ffe4b9acd57829836fbc63693e9ae13
Notkyn <notky@ukr.net>
2019-08-04 00:02:40 +0300

    HW03 - Create scripts for Meals table

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,21 +5,25 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
+    private String description;
+    private int calories;
+    private int userId;
 
-    private final String description;
-
-    private final int calories;
-
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    public Meal(LocalDateTime dateTime, String description, int calories, int userId) {
+        this(null, dateTime, description, calories, userId);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories, int userId) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public LocalDateTime getDateTime() {
@@ -40,6 +44,22 @@ public class Meal extends AbstractBaseEntity {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
     @Override
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(description, datetime, calories, userid) VALUES
+  ('Админ ланч',make_timestamp(2015, 6, 1, 14, 0, 0), 510, 100001),
+  ('Админ ужин',make_timestamp(2015, 6, 1, 21, 0, 0), 1500, 100001),
+  ('Завтрак',make_timestamp(2015, 5, 30, 10, 0, 0), 500, 100000),
+  ('Обед',make_timestamp(2015, 5, 30, 13, 0, 0), 1000, 100000),
+  ('Ужин',make_timestamp(2015, 5, 30, 20, 0, 0), 500, 100000),
+  ('Завтрак',make_timestamp(2015, 5, 10, 14, 0, 0), 1000, 100000),
+  ('Обед',make_timestamp(2015, 5, 31, 13, 0, 0), 500, 100000),
+  ('Ужин',make_timestamp(2015, 5, 31, 20, 0, 0), 510, 100000);
