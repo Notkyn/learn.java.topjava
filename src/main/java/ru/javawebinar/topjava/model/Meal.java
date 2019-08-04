d91b6ad16c6a770c43ff11e297c561e0e43fc518
Notkyn <notky@ukr.net>
2019-08-05 00:01:40 +0300

    HW03 - JdbcMealRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -8,22 +8,20 @@ public class Meal extends AbstractBaseEntity {
     private LocalDateTime dateTime;
     private String description;
     private int calories;
-    private int userId;
 
-    public Meal(LocalDateTime dateTime, String description, int calories, int userId) {
-        this(null, dateTime, description, calories, userId);
+    public Meal() {
+        super();
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories, int userId) {
+    public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(null, dateTime, description, calories);
+    }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.userId = userId;
-    }
-
-    public int getUserId() {
-        return userId;
     }
 
     public LocalDateTime getDateTime() {
@@ -56,10 +54,6 @@ public class Meal extends AbstractBaseEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
 
     @Override
